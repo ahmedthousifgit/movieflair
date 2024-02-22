e7933a22adcc035402e6a9dc199044a1cd1ff2dc
ahmed thousif <ahmedthousif571@gmail.com>
2024-02-22 12:02:36 +0530

    header section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+const API_URL = "http://www.omdbapi.com?apiKey=1f451356";
 
-function App() {
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieFlair</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onCanPlay={() => {}} />
+      </div>
+
+      <div className="container"></div>
     </div>
   );
-}
+};
 
 export default App;
+
+// 1f451356 api key
